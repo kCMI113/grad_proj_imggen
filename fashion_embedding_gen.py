78e4c735e1a353b65f8d736ede3527f203809791
kCMI113 <cmkang1316@gmail.com>
2024-10-14 15:59:42 +0900

    chore: modi some file path, format, etc

--- fashion_embedding_gen.py
+++ fashion_embedding_gen.py
@@ -1,8 +1,9 @@
-from fashion_clip.fashion_clip import FashionCLIP
 import os
-import torch
-import numpy as np
 import random
+
+import numpy as np
+import torch
+from fashion_clip.fashion_clip import FashionCLIP
 from tqdm import tqdm
 
 IMAGES_PATH = "./dataset_fashion/images"
@@ -20,19 +21,20 @@ def seed_everything(seed: int = 42):
 def main():
     fclip = FashionCLIP('fashion-clip')
     
-    images_list = [os.path.join(IMAGES_PATH, image_id, image_id + f"_{i}.jpg") for image_id in os.listdir(IMAGES_PATH) for i in range(1, 4)]
+    images_list = [f'{IMAGES_PATH}/basic_{j}/item_{i}.jpg' for j in range(0, 3) for i in range(0,26235)]
     
-    image_embeddings = fclip.encode_images(images_list, batch_size=128)
+    image_embeddings = fclip.encode_images(images_list, batch_size=8096)
     
     print(f"size of image embeddings : {image_embeddings.shape}")
     torch.save(image_embeddings,os.path.join(EMBEDDINGS_PATH, "embeddings.pth"))
     
     image_embeddings = image_embeddings.reshape(-1, 3, 512)
+    print(f"size of image embeddings : {image_embeddings.shape}")
     
     print(image_embeddings.shape)
     
-    for i, image_id in enumerate(tqdm(os.listdir(IMAGES_PATH))):
-        torch.save(image_embeddings[i], os.path.join(EMBEDDINGS_PATH, image_id + ".pth"))
+    for image_id in range(26235):
+        torch.save(image_embeddings[image_id], f"{EMBEDDINGS_PATH}/{image_id}.pth")
 
 if __name__ == '__main__':
     seed_everything()
